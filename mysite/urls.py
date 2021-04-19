4fd583f4db485b9e5eb5c5df3a24fda421a5acc4
MinSeok Kim <ms_0147@khu.ac.kr>
2021-04-19 23:46:34 +0900

    admin page added

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
